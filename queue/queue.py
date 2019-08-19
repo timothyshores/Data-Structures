efe75cd04595ad6aa2a7c11ebd67aea612f8f03b
Timothy Shores <timothymshores@gmail.com>
2019-08-19 17:54:36 -0500

    Work in progress

--- queue/queue.py
+++ queue/queue.py
@@ -136,15 +136,16 @@ class Queue:
         self.size = 0
         # what data structure should we
         # use to store queue elements?
-        # self.storage =
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, item):  # add an item to the back of the queue.
         self.size += 1
+        self.storage.add_to_tail(item)
 
     # remove and return an item from the front of the queue.
     def dequeue(self):
         self.size -= 1
-        # pass
+        first = self.storage.head
 
     def len(self):
         return self.size
