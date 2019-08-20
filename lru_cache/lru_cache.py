0adaab8940cdb610642ffbd5a48e65cab5ba2d6c
Timothy Shores <timothymshores@gmail.com>
2019-08-20 16:30:48 -0500

    Work in progress

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from dll import DoublyLinkedList
+
+
 class LRUCache:
     # Need to store key value pairs. E.g. dictionary
     # Need to track order
@@ -6,11 +9,8 @@ class LRUCache:
     def __init__(self, limit=10):
         self.cache = DoublyLinkedList()
         self.limit = limit
-
-    def __repr__(self):
-        return f"self.contents: {self.contents}"
-
     """
+    
   Retrieves the value associated with the given key. Also
   needs to move the key-value pair to the top of the order
   such that the pair is considered most-recently used.
@@ -18,28 +18,45 @@ class LRUCache:
   key-value pair doesn't exist in the cache. 
   """
 
+    def __str__(self):
+        s = ""
+        current = self.cache.head
+        while current:
+            s += str(current.value)
+            current = current.next
+        return s
+
     def get(self, key=None):
-        return self.cache
+        if str(list(self.cache.head.value)[0]) == str(key):
+            return "Key found"
+        else:
+            return "Key not found"
+        # return str(list(self.cache.head.value)[0])
 
     """
   Adds the given key-value pair to the cache. The newly-
   added pair should be considered the most-recently used
-  entry in the cache. If the cache is already at max capacity
+  entry in the cache. 
+  
+  If the cache is already at max capacity
   before this entry is added, then the oldest entry in the
-  cache needs to be removed to make room. Additionally, in the
+  cache needs to be removed to make room. 
+  
+  Additionally, in the
   case that the key already exists in the cache, we simply 
   want to overwrite the old value associated with the key with
   the newly-specified value. 
   """
 
     def set(self, key, value):
+        if len(self.cache) == self.limit:
+            self.cache.remove_from_tail()
+
         self.cache.add_to_head({key, value})
-        # self.contents = [{key, value}] + self.contents
-
-        # if (len(self.contents) >= self.limit):
-        #     del self.contents[-1]
 
 
-l1 = LRUCache()
-l1.set("some key 1", "some value 1")
+l1 = LRUCache(2)
 print(l1)
+l1.set("1", "some value 1")
+print(l1)
+print(l1.get(1))
