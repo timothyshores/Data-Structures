9d9f0138147686b5c91d99b17ed2d4e0daf5621c
Timothy Shores <timothymshores@gmail.com>
2019-08-20 11:50:38 -0500

    Import dll.py into queue.py

---
+++ lecture_notes/quees.py
@@ -0,0 +1,5 @@
+two pointers
+one pointer goes to the end of the list, one stays at the start of the list
+swap last element in the list with the first element
+reset points at 2 nd element and 2 nd to last element and swap
+continue until pointers converge at the midpoint
